3a1a627ffd9cceb2e08dccfaec0bb576c4029303
max-m <maximilian.mader@gmx.de>
2011-09-04 22:04:53 +0200

    Little even more senseless commit

--- Minecraft PHP/MinecraftPackets.class.php
+++ Minecraft PHP/MinecraftPackets.class.php
@@ -6,21 +6,35 @@
 		public function construct() {
 			$sock = new SocketManager();
 		}
+		
 		/*
 		 * This function sends a handshake to the server.
 		 * The handshake must be sent before the login package (Packet1) 
 		 */
 		public function Packet2Write($username) {
 			$package  = chr(2); //Packet prefix
-			$package .= str16($username);
+			$package .= self::writeStr16($username);
 			
-			//SocketManager write function should be here ...
+			$sock->write($package);
 		}
 		
-		public static function str16($data) {
+		/*
+		 * This functions reads the Handshake package sent from server as answer to its handshake packet.
+		 */
+		public function Packet2Read($data) {
+			$return = substr($data, 1); //strip packet id
+			$return = self::readStr16($return);
+			return $return;
+		}
+		
+		public static function writeStr16($data) {
 			$return  = pack("n", strlen($data));
 			$return .= mb_convert_encoding($data, "UCS-2LE");
 			return $return;
+		}
+		
+		public static function readStr16($data) {
+			return $data;
 		}
 	}
 ?>
