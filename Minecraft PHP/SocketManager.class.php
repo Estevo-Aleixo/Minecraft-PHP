88219a8f6f8243ebb72cc1729095bb421de4a746
kurtextrem <kurtextrem@gmail.com>
2011-09-06 15:41:33 +0300

    bored

--- Minecraft PHP/SocketManager.class.php
+++ Minecraft PHP/SocketManager.class.php
@@ -1,16 +1,27 @@
 <?php 
 	class SocketManager {
-		public function construct($config) {
+		
+		/**
+		* Initiates the socket
+		*
+		* @param string $config
+		*/
+		public function __construct($config) {
 			$this->initSocket();
 		}
 		
 		/**
 		* Creates a new socket
 		*/
-		public function initSocket() {
+		private function initSocket() {
 			$this->socket = socket_create(AF_INET, SOCK_STREAM, getprotobyname('tcp'));
 		}
 		
+		/**
+		* Writes data to the socket
+		* 
+		* @param string $data
+		*/
 		public function write($data) {
 			socket_write($this->socket, $data, strlen($data));
 		}
