7be1dab76b7679985cb72bbc58a43203051b64aa
max-m <maximilian.mader@gmx.de>
2011-09-29 17:06:06 +0200

    Added the missing packets. The functions are empty!

---
+++ packet/Packet61.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace de\wbbaddons\minecraft\api\packet;
+use de\wbbaddons\minecraft\api\util\DataUtil;
+use de\wbbaddons\minecraft\api\MinecraftPHP;
+
+class Packet61 implements Packet {
+	public static function writePacketData($data) {
+
+	}
+
+	public static function readPacketData() {
+
+	}
+}
